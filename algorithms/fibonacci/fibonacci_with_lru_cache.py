9ee07824cf77176d67b533abef4d05a3f2129f87
booboy <bpmcdevitt@thelinuxspace.com>
2018-01-12 01:40:46 -0600

    added one last change to check to make sure arg n is a positive int

--- algorithms/fibonacci/fibonacci_with_lru_cache.py
+++ algorithms/fibonacci/fibonacci_with_lru_cache.py
@@ -1,8 +1,16 @@
 #!/usr/bin/env python3
 from functools import lru_cache
 
+
 @lru_cache(maxsize = 1000)
 def fibonacci(n):
+    # check input type. must be positive integer.
+    if type(n) != int:
+        raise TypeError("n must be a positive int")
+    if n < 1:
+        raise ValueError("n must be a positive int")
+
+    # compute the nth term
     if n == 1:
         return 1
     elif n == 2:
